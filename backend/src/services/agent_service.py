a4154d6ea4b9eb8212efb06f7fbbd4b92fafd775
lucabozzetti <bozzettiluca33@gmail.com>
2025-09-08 00:57:59 +0200

    geht jetzt

--- backend/src/services/agent_service.py
+++ backend/src/services/agent_service.py
@@ -175,7 +175,7 @@ class AgentService:
                 coding_task = self.coding_agent.run(
                     user_id=user_id,
                     state=self.state_manager.get_state(user_id=user_id, course_id=course_id),
-                    content=self.query_service.get_explainer_query(user_id, course_id, idx),
+                    content=self.query_service.get_explainer_query(user_id, course_id, idx, request.language, request.difficulty),
                 )
 
                 image_task = self.image_agent.run(
@@ -218,7 +218,7 @@ class AgentService:
                 response_tester = await self.tester_agent.run(
                     user_id=user_id,
                     state=self.state_manager.get_state(user_id=user_id, course_id=course_id),
-                    content=self.query_service.get_tester_query(user_id, course_id, idx, response_code["explanation"]),
+                    content=self.query_service.get_tester_query(user_id, course_id, idx, response_code["explanation"], request.language, request.difficulty)
                 )
 
                 # Save questions in db
